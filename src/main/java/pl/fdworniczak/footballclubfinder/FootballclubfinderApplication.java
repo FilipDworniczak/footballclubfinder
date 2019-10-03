7de3e991978a7fbf084f63ff071186022612eee0
Filip Dworniczak <FilipDworniczak@gmail.com>
2019-10-03 11:21:15 +0200

    Initial commit

---
+++ src/main/java/pl/fdworniczak/footballclubfinder/FootballclubfinderApplication.java
@@ -0,0 +1,13 @@
+package pl.fdworniczak.footballclubfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballclubfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballclubfinderApplication.class, args);
+	}
+
+}
